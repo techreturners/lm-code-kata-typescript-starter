57110b5eb2e75987c90b8cce8f92d770a6204616
neil-hughes <neil.hughes@techreturners.com>
2022-05-03 09:12:35 +0100

    Added nodemon and start script for convenience

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+console.log('Your code here...');
+
 class Calc {
-    add(a: number, b: number): number {
-        return a + b;
-    }
+	add(a: number, b: number): number {
+		return a + b;
+	}
 }
 export default new Calc();
